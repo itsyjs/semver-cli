39c74f35b2f36cd4e6a81298c782cb001c3d2bd0
Dave Honneffer <pearofducks@gmail.com>
2024-11-29 12:42:29 +0100

    v1

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 npx @itsy/semver-cli --command major 1.0.1
 ```
 
-- `command` is any single valid method from the [semver library](https://github.com/npm/node-semver)
+- `--debug` can be specified to output debugging info - useful when handling multiple commands
+- `--command` is any single valid method from the [semver library](https://github.com/npm/node-semver)
+  - multiple methods can be specified and will run in order `npx @itsy/semver-cli --command coerce --command valid 1.0.1`
 - the primary argument can be an array of inputs
   - meaning this would work `npx @itsy/semver-cli --command satisfies 1.0.1 1.x`
--- bin.js
+++ bin.js
@@ -4,11 +4,18 @@ import semver from 'semver'
 import arg from 'arg'
 
 const args = arg({
-  '--command': String
+  '--command': [String],
+  '--debug': Boolean
 })
 
-const versionArgs = args._
-const command = args['--command']
-const result = semver[command](...versionArgs)
+const debugging = args['--debug']
+const commands = args['--command']
 
+const pipe = (...fns) => (x) => fns.reduce((_v, f) => {
+  const v = Array.isArray(_v) ? _v : [_v]
+  if (debugging) console.log('Calling', `semver.${f}`, 'with arguments:', v)
+  return semver[f](...v)
+}, x);
+
+const result = pipe(...commands)(args._)
 console.log(result)
